ee0de09ee0ee0037b009b8677774d195bf226623
Les Ansley <les@ansleyfamily.com>
2022-11-17 15:27:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import '../box-styles.css'
+
+const Box = ({size, className = '', style, ...otherProps}) => {
+  const sizeClassName = size ? `box--${size}` : ''
+
+  return (
+    <div
+      className={`box ${sizeClassName} ${className}`}
+      style={{
+        fontStyle: 'italic',
+        ...style,
+      }}
+      {...otherProps}
+    />
+  )
+}
+
+const smallBox = (
+  <div
+    style={{fontStyle: 'italic', backgroundColor: 'lightblue'}}
+    className={`box box--small`}
+  >
+    small lightblue box
+  </div>
+)
+const mediumBox = (
+  <div style={{backgroundColor: 'pink'}} className={`box box--medium`}>
+    medium pink box
+  </div>
+)
+const largeBox = (
+  <div style={{backgroundColor: 'orange'}} className={`box box--large`}>
+    large orange box
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box
+        style={{backgroundColor: 'lightblue'}}
+        className="box box--small"
+        children="small lightblue box"
+      />
+      <Box
+        style={{backgroundColor: 'lightblue'}}
+        size="small"
+        children="small lightblue box"
+      />
+      {smallBox}
+      <Box
+        style={{backgroundColor: 'pink'}}
+        size="medium"
+        children="medium pink box"
+      />
+      {mediumBox}
+      {largeBox}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
